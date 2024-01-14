82703b9b03f7b186acc1084a39eedd49ad23c600
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2024-01-14 22:21:40 +0530

    Exploratory Data Analysis

--- model/pickles/test.py
+++ model/pickles/test.py
@@ -1,23 +1,20 @@
 import pickle
-string = input('Enter mail: ')
-
 from nltk.stem import PorterStemmer
 stemmer = PorterStemmer()
 
-def stem_sentence(sentence):
+def checkSpam(string):
     new=""
-    for word in sentence.split(" "):
-        new+=stemmer.stem(word.strip(',').strip('!').strip(' '))+" "
-    return new
+    for word in string.split(" "):
+        new+=stemmer.stem(word.strip(' '))+" "
+    
+    fm = open("E:\Study\OneDrive - Manipal Academy of Higher Education\Coding\Mini-Projects\Spam-Email-Detection\model\pickles\model_pickle",'rb')
+    model = pickle.load(fm)
+    fv = open("vectorizer_pickle",'rb')
+    vectorizer = pickle.load(fv)
 
-fm = open("E:\Study\OneDrive - Manipal Academy of Higher Education\Coding\Mini-Projects\Spam-Email-Detection\model\pickles\model_pickle",'rb')
-model = pickle.load(fm)
+    check = vectorizer.transform([new])
+    return model.predict(check)[0]
 
-fv = open("vectorizer_pickle",'rb')
-vectorizer = pickle.load(fv)
-
-string = stem_sentence(string)
-print('\n\n',string)
-check = vectorizer.transform([string])
-print(model.predict(check)[0])
-
+if __name__=='__main__':
+    string = input('Enter mail: ')
+    print(checkSpam(string))
